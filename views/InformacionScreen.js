faa31a4ca07f26bcaac57b4b8d77039240962d8d
Manuelmsc <mscmmagdaleno2022@accitesz.com>
2023-06-09 15:34:14 -0600

    Actualizacion 0334 09062023

--- views/InformacionScreen.js
+++ views/InformacionScreen.js
@@ -1,11 +1,18 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text,Image } from "react-native";
+
 
 export default function InformacionScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Pantalla Informacion Medica</Text>
-      <Text>Contenido</Text>
+    <View >
+
+    <Image source={{uri: 'https://www.bbva.ch/wp-content/uploads/2021/07/recurso_-tipos-de-alimentacion.jpg'}}
+       style={{width: 300, height: 200}} />
+    
+    <Image source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCmLF7RlbEQG5Wqps4bMCaB4LR0iiq6RrIYQ&usqp=CAU'}}
+       style={{width: 300, height: 200}} />
+    <StatusBar style="auto" />
     </View>
   );
 }
