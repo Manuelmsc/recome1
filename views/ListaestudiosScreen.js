853d169e9fb5e67e7917ba6d0502ce2b026980ec
Manuelmsc <mscmmagdaleno2022@accitesz.com>
2023-06-09 23:47:41 -0600

    actualizacion 11 47 pm 09062023

--- views/ListaestudiosScreen.js
+++ views/ListaestudiosScreen.js
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 import { Icon,List,Item } from '@rneui/themed';
 import { useNavigation } from '@react-navigation/native';
 import { ListItem } from "@react-native-material/core";
+//import { ListItem } from 'react-native-elements';
 
 
 export default function ListaestudiosScreen() {
@@ -10,6 +11,17 @@ export default function ListaestudiosScreen() {
   navigation.setOptions({ title: 'Lista de Estudios'  })
 
     return (
+      /*<View tyle={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
+        <ListItem.Content style={{flexDirection: 'row'}}>
+          <View style={{width: '50%'}}>
+            <Text>Nombre.</Text>
+          </View>
+          <View style={{width: '50%'}}>
+            <Text>Este es el segundo campo de texto.</Text>
+          </View>
+        </ListItem.Content>
+      </View>*/
+
       <View >
           <ListItem
             title="Jaime Eliazar Esparza"
